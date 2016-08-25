db8e656730995e82f9c5393e39e0fb03e1c33d3c
Leonid Shevtsov <leonid@shevtsov.me>
2016-08-25 15:30:16 +0300

    replace fork with Process.spawn

--- lib/headless/cli_util.rb
+++ lib/headless/cli_util.rb
@@ -40,12 +40,7 @@ class Headless
     end
 
     def self.fork_process(command, pid_filename, log_filename='/dev/null')
-      pid = fork do
-        STDERR.reopen(log_filename)
-        exec command
-        exit! 127 # safeguard in case exec fails
-      end
-
+      pid = Process.spawn(command, err: log_filename)
       File.open pid_filename, 'w' do |f|
         f.puts pid
       end
